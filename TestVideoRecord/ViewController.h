ff8f53d0c746f6655c3831e875d673b6789c5677
wyhazq <wyhazq@gmail.com>
2016-08-18 14:14:47 +0800

    Initial Commit

---
+++ TestVideoRecord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestVideoRecord
+//
+//  Created by wyh on 16/8/18.
+//  Copyright © 2016年 wyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestVideoRecord/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestVideoRecord
+//
+//  Created by wyh on 16/8/18.
+//  Copyright © 2016年 wyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
